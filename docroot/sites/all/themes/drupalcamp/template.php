9df8ff349c1138e5a2edba02662ef7da6ecf8452
ssudhak <ssudhak@its.jnj.com>
2015-06-24 17:33:14 +0530

    menu links

--- docroot/sites/all/themes/drupalcamp/template.php
+++ docroot/sites/all/themes/drupalcamp/template.php
@@ -5,3 +5,18 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * drupalcamp theme.
  */
+
+function drupalcamp_menu_link(array $variables) {
+	global $user;
+	if ($variables ['element']['#title'] == 'My account' && $variables ['element']['#href'] == 'user'){
+		$variables ['element']['#href'] = 'user/'.$user->uid.'/edit';
+	}
+	$element = $variables ['element'];
+	$sub_menu = '';
+
+	if ($element ['#below']) {
+	$sub_menu = drupal_render($element ['#below']);
+	}
+	$output = l($element ['#title'], $element ['#href'], $element ['#localized_options']);
+	return '<li' . drupal_attributes($element ['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
