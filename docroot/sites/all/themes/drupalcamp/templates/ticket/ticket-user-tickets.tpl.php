87bb9ba58fce74f8edead187ce81b9d39c7f3220
hw <hussainweb@gmail.com>
2015-07-11 14:52:14 +0530

    Style for ticket registration pages.

---
+++ docroot/sites/all/themes/drupalcamp/templates/ticket/ticket-user-tickets.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * A basic template for user ticket
+ *
+ * Available variables:
+ *   TODO: Add documentation.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_entity()
+ * @see template_process()
+ */
+?>
+
+<div class="container-inner">
+  <h2 class="ticket-user-tickets-title"><?php print $title; ?></h2>
+  <div class="ticket-user-tickets-data"><?php print $data; ?></div>
+</div>
